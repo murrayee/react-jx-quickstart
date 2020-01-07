c33c99dc10fe0d0989c4d3f74fc9f91d1fa01c56
murray <murrayee@163.com>
2020-01-07 17:19:00 +0800

    axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    text: "123"
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://foxcore.jx-staging.zxy5.xyz/weatherforecast")
+      .then(res => {
+        this.setState({
+          text: res.data
+        });
+      });
+  }
   render() {
-    return <div className="App">hello world</div>;
+    return <div className="App">hello world {this.state.text}</div>;
   }
 }
 
